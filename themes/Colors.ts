2fc682f59a74ac773d3b90efd499551324c630c1
Fadil Hisyam <ichorvfx@gmail.com>
2024-05-30 03:57:04 +0700

    Add Themes

---
+++ themes/Colors.ts
@@ -0,0 +1,19 @@
+export const Colors = {
+    white: "#F5F6FB",
+    lightGray: "#E5E8F5",
+    accent: "#484CFB",
+    gray: "#838694",
+    black: "#101016",
+    primary: "#3539DB",
+    primaryDark: "#32327E",
+    secondary: "#E2E3FF",
+    error: {
+        primary: "#FF1A1A",
+        light: "#FFEFEF",
+    },
+    info: {
+        primary: "#398EDC",
+        light: "#F6FBFF",
+    },
+    shadow: "#14152708",
+}
