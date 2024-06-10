19459c0b65820ff31e80c2d9d05e2c13031507d8
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-10 09:26:17 +0700

    Added a touch on blank to close logic

--- components/Modal/ImageModal.tsx
+++ components/Modal/ImageModal.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react"
-import { Modal, View, Image, ScrollView, ActivityIndicator } from "react-native"
+import {
+  Modal,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native"
 import StyledIconButton from "../StyledIconButton"
 import { XMarkIcon } from "react-native-heroicons/solid"
 import { Colors } from "../../themes/Colors"
@@ -23,49 +32,84 @@ const ImageModal = ({ imageModalVisible, setImageModalVisible, url }) => {
       onRequestClose={() => setImageModalVisible(!imageModalVisible)}
     >
       <ScrollView
+        showsVerticalScrollIndicator={false}
         contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
         style={{
           backgroundColor: "rgba(0, 0, 0, 0.75)",
         }}
       >
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 16,
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          activeOpacity={1}
+          onPressOut={() => {
+            setImageModalVisible(false)
           }}
         >
-          <View style={{ position: "relative", width: "100%", elevation: 1 }}>
-            {aspectRatio ? (
-              <Image
-                style={{
-                  width: "100%",
-                  height: undefined,
-                  aspectRatio: aspectRatio,
-                  borderRadius: 8,
-                }}
-                source={{ uri: url }}
-                resizeMode="contain"
-              />
-            ) : (
-              <ActivityIndicator size="large" color={Colors.white} />
-            )}
-            <StyledIconButton
-              style={{
-                position: "absolute",
-                top: 8,
-                left: 8,
-                backgroundColor: "#00000060",
-                borderRadius: 8,
-              }}
-              width={36}
-              onPress={() => setImageModalVisible(!imageModalVisible)}
-            >
-              <XMarkIcon color={Colors.white} />
-            </StyledIconButton>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 16,
+            }}
+          >
+            <TouchableWithoutFeedback touchSoundDisabled>
+              <View
+                style={{ position: "relative", width: "100%", elevation: 1 }}
+              >
+                {aspectRatio ? (
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: undefined,
+                      aspectRatio: aspectRatio,
+                      borderRadius: 8,
+                    }}
+                    source={{ uri: url }}
+                    resizeMode="contain"
+                  />
+                ) : (
+                  <ActivityIndicator size="large" color={Colors.white} />
+                )}
+                <StyledIconButton
+                  style={{
+                    position: "absolute",
+                    top: 8,
+                    left: 8,
+                    backgroundColor: "#00000060",
+                    borderRadius: 8,
+                  }}
+                  width={36}
+                  onPress={() => setImageModalVisible(!imageModalVisible)}
+                >
+                  <XMarkIcon color={Colors.white} />
+                </StyledIconButton>
+                <TouchableWithoutFeedback
+                  onPress={() => setImageModalVisible(false)}
+                >
+                  <View
+                    style={{
+                      display: "flex",
+                      padding: 16,
+                      alignItems: "center",
+                      width: "100%",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "Nunito-Regular",
+                        fontSize: 16,
+                        color: "#F5F6FBCA",
+                      }}
+                    >
+                      Tekan area kosong untuk keluar
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+            </TouchableWithoutFeedback>
           </View>
-        </View>
+        </TouchableOpacity>
       </ScrollView>
     </Modal>
   )
