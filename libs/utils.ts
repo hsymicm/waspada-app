6604e594f4a779266d9d2c6693c88e7f1d4a5029
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-10 09:26:52 +0700

    Refactor old functions

--- libs/utils.ts
+++ libs/utils.ts
@@ -7,16 +7,16 @@ export const scrollTop = (ref: any) => {
 }
 
 export const handleAuthErrorMessage = (message?: string) => {
-  const defaultMsg = "Error, something wrong happened";
+  const defaultMsg = "Error, something wrong happened"
 
   if (!message) {
-    return defaultMsg;
+    return defaultMsg
   }
 
   const parts = message.split("/")
 
   if (parts[0] !== "auth") {
-    return defaultMsg;
+    return defaultMsg
   }
 
   const str = parts[1].replaceAll("-", " ")
@@ -25,55 +25,77 @@ export const handleAuthErrorMessage = (message?: string) => {
 
 export const formatRatingCounter = (count: number) => {
   if (count >= 1000000) {
-      const millions = Math.floor(count / 1000000);
-      const remainder = Math.floor((count % 1000000) / 100000);
-      return `${millions}.${remainder}M`;
+    const millions = Math.floor(count / 1000000)
+    const remainder = Math.floor((count % 1000000) / 100000)
+    return `${millions}.${remainder}M`
   } else if (count >= 1000) {
-      const thousands = Math.floor(count / 1000);
-      const remainder = Math.floor((count % 1000) / 100);
-      return `${thousands}.${remainder}K`;
+    const thousands = Math.floor(count / 1000)
+    const remainder = Math.floor((count % 1000) / 100)
+    return `${thousands}.${remainder}K`
   } else {
-      return count.toString();
+    return count.toString()
   }
 }
 
 export const formatElapsedTime = (timestamp: any) => {
-  const current = new Date();
-  const timestampDate = timestamp.toDate();
-  const elapsedTime = current - timestampDate;
-  const seconds = Math.floor(elapsedTime / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+  const current: any = new Date()
+  const timestampDate = timestamp.toDate()
+  const elapsedTime = current - timestampDate
+  const seconds = Math.floor(elapsedTime / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
 
   if (hours >= 24) {
-      const days = Math.floor(hours / 24);
-      return `${days} hari yang lalu`; // "x days ago"
+    const days = Math.floor(hours / 24)
+    return `${days} hari yang lalu`
   } else if (hours >= 1) {
-      return `${hours} jam yang lalu`; // "x hours ago"
+    return `${hours} jam yang lalu`
   } else if (minutes >= 1) {
-      return `${minutes} menit yang lalu`; // "x minutes ago"
+    return `${minutes} menit yang lalu`
   } else {
-      return "Baru saja"; // "Just now" for less than a minute
+    return "Baru saja" 
   }
 }
 
-export const formatTimestamp = (timestamp: any) => {
-  const date = timestamp.toDate();
+export const parseMetadataTimestamp = (date: string) => {
+  const [datePart, timePart] = date.split(" ")
+  const [year, month, day] = datePart.split(":").map(Number)
+  const [hours, minutes, seconds] = timePart.split(":").map(Number)
+
+  const parsedDate = new Date(year, month - 1, day, hours, minutes, seconds)
+  return parsedDate
+}
+
+export const formatTimestamp = (date: any) => {
   const monthNames = [
-    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
-    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
-  ];
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ]
 
-  const day = date.getDate();
-  const month = monthNames[date.getMonth()];
-  const year = date.getFullYear();
-  
-  let hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  
-  hours = hours % 12;
-  hours = hours ? hours : 12;
+  const day = date.getDate()
+  const month = monthNames[date.getMonth()]
+  const year = date.getFullYear()
 
-  return `${month} ${day}, ${year} - ${hours}:${minutes} ${ampm}`;
+  let hours = date.getHours()
+  const minutes = date.getMinutes().toString().padStart(2, "0")
+  const ampm = hours >= 12 ? "PM" : "AM"
+
+  hours = hours % 12
+  hours = hours ? hours : 12
+
+  return `${month} ${day}, ${year} - ${hours}:${minutes} ${ampm}`
+}
+
+export const kMToLongitudes = (km: number, atLatitude: number) => {
+  return (km * 0.0089831) / Math.cos(atLatitude * (Math.PI / 180))
 }
