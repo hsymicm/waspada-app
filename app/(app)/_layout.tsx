2d92a9519a6cd6f04080ec723ae23da17a054a9c
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-10 09:13:55 +0700

    Change drawer screen for addpost

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Slot } from "expo-router"
+import { Redirect } from "expo-router"
 import { useEffect, useState } from "react"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
@@ -7,20 +7,20 @@ import Header from "../../components/Header"
 import ContentFilter from "../../components/ContentFilter"
 import CustomDrawer from "../../components/CustomDrawer"
 
-const checkFirstLogin = async () => {
-  try {
-    const value = JSON.parse(await AsyncStorage.getItem("not_first_time"))
-    return value ? value : null
-  } catch (error) {
-    console.log(error)
-    return null
-  }
-}
-
 export default function AppLayout() {
   const [notFirstTime, setNotFirstTime] = useState(null)
   const [loading, setLoading] = useState(true)
   const [search, setSearch] = useState("")
+
+  const checkFirstLogin = async () => {
+    try {
+      const value = JSON.parse(await AsyncStorage.getItem("not_first_time"))
+      return value ? value : null
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
 
   useEffect(() => {
     const checkStatus = async () => {
@@ -41,8 +41,8 @@ export default function AppLayout() {
         screenOptions={{
           drawerPosition: "right",
         }}
-        drawerContent={({ navigation }) => (
-          <CustomDrawer navigation={navigation} />
+        drawerContent={({ navigation, state }) => (
+          <CustomDrawer navigation={navigation} state={state} />
         )}
       >
         <Drawer.Screen
@@ -73,12 +73,9 @@ export default function AppLayout() {
           }}
         />
         <Drawer.Screen
-          name="(auth)/addpost"
+          name="(auth)"
           options={{
-            unmountOnBlur: true,
-            header: ({ navigation }) => (
-              <Header title="Lapor Kecelakaan" navigation={navigation} />
-            ),
+            headerShown: false,
           }}
         />
       </Drawer>
