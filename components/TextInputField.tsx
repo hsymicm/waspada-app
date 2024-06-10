d021d6f3d2a663220d2035ac480bf62c291e1176
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-10 09:25:21 +0700

    Added new props (textArea, autoCapitalize, disabled)

--- components/TextInputField.tsx
+++ components/TextInputField.tsx
@@ -1,38 +1,47 @@
 import { StyleSheet, TextInput, View, TouchableOpacity } from "react-native"
 import { Colors } from "../themes/Colors"
-import { EyeIcon, EyeSlashIcon } from "react-native-heroicons/solid"
 
 import { useState } from "react"
 import { KeyboardTypeOptions } from "react-native"
 
 interface TextInputFieldProps {
   type?: KeyboardTypeOptions
+  textArea?: boolean
   placeholder?: string
   value?: string
   setValue?: (arg0: string) => void
   password?: boolean
+  autoCapitalize?: boolean
+  disabled?: boolean
   style?: object
 }
 
 export default function TextInputField({
   type,
+  textArea,
   placeholder,
   value,
   setValue,
   password,
   style,
+  disabled,
+  autoCapitalize = true,
 }: TextInputFieldProps) {
   const [isActive, setActive] = useState(false)
   return (
     <TextInput
       placeholder={placeholder}
       secureTextEntry={password}
+      multiline={textArea}
+      autoCapitalize={autoCapitalize ? "sentences" : "none"}
+      numberOfLines={textArea ? 4 : 1}
+      textAlignVertical={textArea ? "top" : "auto"}
       style={[
         styles.input,
-        (isActive || value) && { borderColor: Colors.accent },
-        isActive && {
-          elevation: 8,
-          shadowColor: "#7B7EFF",
+        (isActive && !disabled)  && { borderColor: Colors.accent },
+        disabled && { backgroundColor: "#EAEBF0" },
+        textArea && {
+          paddingVertical: 14,
         },
         style,
       ]}
@@ -41,6 +50,7 @@ export default function TextInputField({
       keyboardType={type}
       onFocus={() => setActive(true)}
       onBlur={() => setActive(false)}
+      readOnly={disabled}
       selectionColor={Colors.accent}
     />
   )
