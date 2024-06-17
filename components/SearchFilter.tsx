3fdc49834006d77eb7af8b209b4b5f37336cd4c2
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-17 22:57:03 +0700

    Added on submit function

--- components/SearchFilter.tsx
+++ components/SearchFilter.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native"
+import { StyleSheet, TextInput, TouchableOpacity, View, Text } from "react-native"
 import {
   MagnifyingGlassIcon as SearchIcon,
   XMarkIcon,
@@ -9,34 +9,57 @@ import { useState } from "react"
 interface SearchFilterProps {
   style?: object
   placeholder?: string
-  value: string
-  setValue: (arg0: string) => void
+  value?: string
+  setValue?: any
+  onSubmit?: any
 }
 
 export default function SearchFilter({
+  placeholder,
   value,
   setValue,
-  placeholder,
+  onSubmit,
   style,
 }: SearchFilterProps) {
   const [isActive, setActive] = useState(false)
+
   return (
     <View style={{ position: "relative", flex: 1 }}>
-        <TextInput
-          placeholder={placeholder}
-          style={[
-            styles.input,
-            isActive && { borderColor: Colors.accent },
-            (value === "" || !value) && { paddingLeft: 44 },
-            style,
-          ]}
-          value={value}
-          onChangeText={setValue}
-          returnKeyType="search"
-          onFocus={() => setActive(true)}
-          onBlur={() => setActive(false)}
-          selectionColor={Colors.accent}
-        />
+      <TextInput
+        placeholder={placeholder}
+        style={[
+          styles.input,
+          isActive && { borderColor: Colors.accent },
+          (value === "" || !value) && { paddingLeft: 44 },
+          style,
+        ]}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="search"
+        onFocus={() => setActive(true)}
+        onBlur={() => setActive(false)}
+        selectionColor={Colors.accent}
+        onSubmitEditing={onSubmit}
+      />
+      <View
+        style={{
+          top: "100%",
+          // bottom: "100%",
+          transform: [{ translateY: 16}],
+          position: "absolute",
+          borderWidth: 1,
+          borderColor: Colors.gray,
+          borderRadius: 16,
+          backgroundColor: Colors.white,
+          elevation: 1,
+        }}
+      >
+        <Text>Hello World!</Text>
+        <Text>Hello World!</Text>
+        <Text>Hello World!</Text>
+        <Text>Hello World!</Text>
+        <Text>Hello World!</Text>
+      </View>
       <View
         style={[
           {
