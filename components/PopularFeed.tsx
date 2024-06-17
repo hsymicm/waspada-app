57955e19f832a4cf8d3900080c0ad1089bcc8c4a
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-17 22:57:18 +0700

    Added Popular Feed Component

---
+++ components/PopularFeed.tsx
@@ -0,0 +1,144 @@
+import { useEffect, useState } from "react"
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native"
+
+import { Colors } from "../themes/Colors"
+import { PlusIcon } from "react-native-heroicons/solid"
+
+import { getPopularReports } from "../models/reportModel"
+import Card from "../components/Card"
+import CardSkeleton from "../components/Skeleton/CardSkeleton"
+import { router } from "expo-router"
+import { Shadow } from "react-native-shadow-2"
+
+export default function PopularFeed({ currentUser }) {
+  const [reports, setReports] = useState([])
+  const [lastReport, setLastReport] = useState(null)
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const [isRefresh, setRefresh] = useState<boolean>(false)
+  
+  const fetchAllReports = async () => {
+    setLoading(true)
+    const res = await getPopularReports.firstBatch()
+    setReports(res.data)
+    setLastReport(res.lastKey)
+    setLoading(false)
+  }
+  const onEnd = async () => {
+    if (!isLoading && lastReport) {
+      setLoading(true)
+      const res = await getPopularReports.nextBatch(lastReport)
+      setReports([...reports, ...res.data])
+      setLastReport(res.lastKey ? res.lastKey : null)
+      setLoading(false)
+    }
+  }
+
+  const onRefresh = async () => {
+    setRefresh(true)
+    setReports([])
+    setLastReport(null)
+    await fetchAllReports()
+    setRefresh(false)
+  }
+
+  useEffect(() => {
+    fetchAllReports()
+  }, [])
+
+  return (
+    <View style={{ position: "relative", flex: 1 }}>
+      <FlatList
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        style={{ backgroundColor: Colors.lightGray }}
+        contentContainerStyle={styles.container}
+        data={reports}
+        onEndReachedThreshold={0.9}
+        onEndReached={onEnd}
+        keyExtractor={(item) => item.uid}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={
+          <CardSkeleton
+            isLoading={isLoading}
+            firstLoad={lastReport ? false : true}
+          />
+        }
+        renderItem={({ item }) => (
+          <Card
+            id={item.uid}
+            rating={item.voteCounter}
+            description={item.description}
+            date={item.date}
+            location={`${item.subdistrict}, ${item.district}, ${item.city}.`}
+            url={item.imageUrl}
+            reportedAlot={item.many}
+          />
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefresh}
+            onRefresh={onRefresh}
+            colors={[Colors.accent]}
+            progressBackgroundColor={Colors.white}
+          />
+        }
+      />
+      {currentUser && (
+        <TouchableOpacity
+          onPress={() => router.navigate("/(auth)/addpost")}
+          activeOpacity={1}
+          style={{ position: "absolute", bottom: 48, right: 36 }}
+        >
+          <Shadow offset={[0, 2]} distance={4} startColor={Colors.shadow}>
+            <View
+              style={{
+                backgroundColor: Colors.primary,
+                padding: 16,
+                borderRadius: 32,
+              }}
+            >
+              <PlusIcon size={28} color={Colors.white} />
+            </View>
+          </Shadow>
+        </TouchableOpacity>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  locationContainer: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+    padding: 12,
+    borderRadius: 16,
+    backgroundColor: Colors.white,
+  },
+  heading: {
+    fontFamily: "Nunito-Bold",
+    fontSize: 20,
+    color: Colors.accent,
+    marginBottom: 2,
+  },
+  subHeading: {
+    fontFamily: "Nunito-Regular",
+    fontSize: 16,
+    color: Colors.black,
+  },
+  text: {
+    fontFamily: "Nunito-Regular",
+    fontSize: 16,
+  },
+  container: {
+    padding: 16,
+    gap: 16,
+  },
+})
