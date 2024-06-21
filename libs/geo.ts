7149a668d02124df8c47d955b4bad03ab35d9c3a
Fadil Hisyam <ichorvfx@gmail.com>
2024-06-21 18:00:53 +0700

    Geocode function - Added the option to use lookup id api url, Added geoAutocomplete and geoAutosuggest function

--- libs/geo.ts
+++ libs/geo.ts
@@ -3,7 +3,7 @@ interface locationType {
   latitude: number
 }
 
-export const revGeocode = async ({latitude, longitude}: locationType) => {
+export const revGeocode = async ({ latitude, longitude }: locationType) => {
   const url = `https://revgeocode.search.hereapi.com/v1/revgeocode?apiKey=${process.env.HERE_API_KEY}&at=${latitude},${longitude}&types=street&lang=id`
 
   try {
@@ -20,8 +20,57 @@ export const revGeocode = async ({latitude, longitude}: locationType) => {
   }
 }
 
-export const geocode = async ({ location }) => {
-  const url = `https://geocode.search.hereapi.com/v1/geocode?q=${encodeURI(location)}&apiKey=${process.env.HERE_API_KEY}`
+export const geocode = async ({ location }: { location: string }) => {
+  const arr = location.split(":")
+  let url: string
+
+  if (arr[0] === "here") {
+    url = `https://lookup.search.hereapi.com/v1/lookup?id=${encodeURI(
+      location
+    )}&apiKey=${process.env.HERE_API_KEY}`
+  } else {
+    url = `https://geocode.search.hereapi.com/v1/geocode?q=${encodeURI(
+      location
+    )}&apiKey=${process.env.HERE_API_KEY}`
+  }
+
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(error?.message || "Error, something happened")
+  }
+}
+
+export const geoAutocomplete = async ({ query, latitude, longitude }) => {
+  const url = `https://autocomplete.search.hereapi.com/v1/autocomplete?q=${encodeURI(
+    query
+  )}&at=${latitude},${longitude}&lang=id&apiKey=${process.env.HERE_API_KEY}`
+
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(error?.message || "Error, something happened")
+  }
+}
+
+export const geoAutosuggest = async ({ query, latitude, longitude }) => {
+  const url = `https://autosuggest.search.hereapi.com/v1/autosuggest?q=${encodeURI(
+    query
+  )}&at=${latitude},${longitude}&lang=id&limit=5&apiKey=${process.env.HERE_API_KEY}`
 
   try {
     const response = await fetch(url)
